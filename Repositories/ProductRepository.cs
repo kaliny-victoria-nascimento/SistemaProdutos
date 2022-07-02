9a8ea37d51e3b6ecf6ee7861c8cac08999fddb8f
Kaliny Victória <kalinyvic05@gmail.com>
2022-07-01 22:30:21 -0300

    arquivo completo, falta alteracoes

--- Repositories/ProductRepository.cs
+++ Repositories/ProductRepository.cs
@@ -5,6 +5,8 @@ using Microsoft.Data.Sqlite;
 
 namespace Avaliacao3BimLp3.Repositories;
 
+
+//nao ta rodando***
 class ProductRepository
 {
     private DatabaseConfig databaseConfig;
@@ -18,15 +20,16 @@ class ProductRepository
     {
         using var connection = new SqliteConnection(databaseConfig.ConnectionString);
         connection.Execute("INSERT INTO Product VALUES(@id, @name, @price, @active)", product);
-
         return product;
     }
 
-
+    public Boolean ExitsById(int id){
+        using var connection = new SqliteConnection(databaseConfig.ConnectionString);
+        return connection.ExecuteScalar<Boolean>("SELECT count(id) FROM Products WHERE Id = @id;",new {id = id});       
+    }
     public void Delete(int id)
     {
         using var connection = new SqliteConnection(databaseConfig.ConnectionString);
-
         connection.Execute("DELETE FROM Product WHERE id = $id", new { Id = id });
     }
 
@@ -35,7 +38,7 @@ class ProductRepository
         using var connection = new SqliteConnection(databaseConfig.ConnectionString);
         connection.Execute("UPDATE products SET active = True WHERE Id = @id", new {id = id});
     }
-
+    //rodando***
     public void Disable(int id)
     {
         using var connection = new SqliteConnection(databaseConfig.ConnectionString);
@@ -46,7 +49,37 @@ class ProductRepository
     {
         using var connection = new SqliteConnection(databaseConfig.ConnectionString);
         var product = connection.Query<Product>("SELECT * FROM Product").ToList();
-
         return product;
     }
+
+    // Retorna os produtos dentro de um intervalo de preço*********NAO TA RODANDO 
+    public List<Product> GetAllWithPriceBetween(double initialPrice, double endPrice)
+    {
+        using var connection = new SqliteConnection(databaseConfig.ConnectionString);
+        var products = connection.Query<Product>("SELECT * FROM products WHERE price > @initialPrice AND price < @endPrice", new {initialPrice = initialPrice, endPrice = endPrice}).ToList();
+        return products;
+    }
+
+    // Retorna os produtos com preço acima de um preço especificado
+    public List<Product> GetAllWithPriceHigherThan(double price)
+    {
+        using var connection = new SqliteConnection(databaseConfig.ConnectionString);
+        var products = connection.Query<Product>("SELECT * FROM products WHERE price > @price", new {price = price}).ToList();
+        return products;
+    }
+
+    public List<Product> GetAllWithPriceLowerThan(double price)
+    {
+        using var connection = new SqliteConnection(databaseConfig.ConnectionString);
+        var products = connection.Query<Product>("SELECT * FROM products WHERE price < @price", new {price = price}).ToList();
+        return products;
+    }
+
+    public double GetAveragePrice()
+    {
+        using var connection = new SqliteConnection(databaseConfig.ConnectionString);                          
+        double avarage = connection.ExecuteScalar<double>("SELECT AVG(price) FROM products");
+        return avarage;
+    }
+
 }
